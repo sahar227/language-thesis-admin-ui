975d7a6275540235f52ce1a2f2065d7f3c16ae20
sahar227 <sahar22798@gmail.com>
2021-05-28 23:15:27 +0300

    Started working on word editor page

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,13 @@
+import WordsEditorPage from "../../Pages/WordsEditorPage/WordsEditorPage";
+import Header from "../Header/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <WordsEditorPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/WordsEditorPage/WordsEditorPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import WordPhaseTabs from "../../Components/WordPhaseTabs/WordPhaseTabs";
+
+export default function WordsEditorPage() {
+  return (
+    <div>
+      <WordPhaseTabs />
+    </div>
+  );
+}
