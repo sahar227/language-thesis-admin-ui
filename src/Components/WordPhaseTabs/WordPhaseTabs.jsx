fa7f9c86a923bd59464cd98b9b32e1d4d5bffc19
sahar227 <sahar22798@gmail.com>
2021-05-28 23:21:45 +0300

    Added initial component for Phase 1 word editor

--- src/Components/WordPhaseTabs/WordPhaseTabs.jsx
+++ src/Components/WordPhaseTabs/WordPhaseTabs.jsx
@@ -6,6 +6,7 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import WordsEditorPhase1 from "../WordsEditorPhase1/WordsEditorPhase1";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -68,7 +69,7 @@ export default function WordPhaseTabs() {
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-        Item One
+        <WordsEditorPhase1 />
       </TabPanel>
       <TabPanel value={value} index={1}>
         Item Two
---
+++ src/Components/WordsEditorPhase1/WordsEditorPhase1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WordsEditorPhase1({ words }) {
+  return (
+    <div>
+      <h1>Words Phase 1</h1>
+    </div>
+  );
+}
